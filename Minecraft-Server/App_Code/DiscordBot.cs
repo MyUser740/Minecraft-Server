05f224102065d59e748c81ff58c5931de778d6f8
axel020207@gmail.com <axel020207@gmail.com>
2020-12-08 18:02:02 +0700

    Discord Bot Manager

---
+++ Minecraft-Server/App_Code/DiscordBot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Minecraft_Server.App_Code
+{
+    public class DiscordBot : HttpApplication
+    {
+        Process dcBot = new Process();
+        public void Start()
+        {
+            string loc = Server.MapPath("..") + @"\Data\Linux\App\discordApp\nodeJS\Launch.bat";
+
+            dcBot.StartInfo.FileName = loc;
+            dcBot.StartInfo.CreateNoWindow = true;
+
+            dcBot.WaitForExit();
+
+        }
+
+        public void Stop()
+        {
+            string loc = Server.MapPath("..") + @"\Data\Linux\App\discordApp\nodeJS\Launch.bat";
+
+            dcBot.StartInfo.FileName = loc;
+
+            dcBot.Close();
+
+        }
+    }
+}
