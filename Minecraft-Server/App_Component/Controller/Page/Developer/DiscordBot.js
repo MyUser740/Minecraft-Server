f69bc4512b990ba348dfdae5f47c4eebbb7ad71c
axel020207@gmail.com <axel020207@gmail.com>
2020-12-09 21:30:47 +0700

    Discord Bot Manager

---
+++ Minecraft-Server/App_Component/Controller/Page/Developer/DiscordBot.js
@@ -0,0 +1,26 @@
+﻿
+app.controller("DeveloperController", function ($scope, $http) {
+    $scope.startBot = function () {
+        $.ajax({
+            method: 'GET',
+            url: '/api/DiscordBot/0',
+            success: function (r) {
+                if (r === 'success') {
+                    Swal.fire(
+                        'Success!',
+                        'That thing is still around?',
+                        'success'
+                    );
+                }
+                else {
+                    var errMsg = r.split('|');
+                    Swal.fire(
+                        'An error occurred',
+                        errMsg[1],
+                        'error'
+                    );
+                }
+            }
+        });
+    }
+});
---
+++ Minecraft-Server/Controllers/DeveloperController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Minecraft_Server.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        // GET: Developer
+        public ActionResult DiscordBot()
+        {
+            return View();
+        }
+    }
+}
