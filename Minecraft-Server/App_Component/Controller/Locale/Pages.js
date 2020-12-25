c8031e74f1741385773344885f26774a2d5968b5
axel020207@gmail.com <axel020207@gmail.com>
2020-12-25 22:59:47 +0700

    Gw g tau knp malah ngebug

--- Minecraft-Server/App_Component/Controller/Locale/Pages.js
+++ Minecraft-Server/App_Component/Controller/Locale/Pages.js
@@ -1,1 +1,11 @@
-﻿
+﻿var lang;
+
+if ($.cookie('stg_lang'))
+    lang = $.cookie('stg_lang')
+else
+    lang = 'en-GB';
+
+var _LayoutRsc;
+readTextFile('./App_Component/Controller/Locale/Script/_Layout/' + lang + '.json', function (text) {
+    _LayoutRsc = JSON.parse(text);
+});
--- Minecraft-Server/App_Component/Controller/Shared/_Layout/Script/ng-Config.js
+++ Minecraft-Server/App_Component/Controller/Shared/_Layout/Script/ng-Config.js
@@ -1,7 +1,15 @@
-﻿app.controller("QuickSearch", function ($scope, $http) {
+﻿
+
+console.log(_LayoutRsc);
+
+app.controller("QuickSearch", function ($scope, $http) {
     $scope.quickSearches = function () {
         if ($('#QuickSearch').val() === '')
-            swal.fire();
+            Swal.fire(
+                _LayoutRsc.Texts[25].text,
+                _LayoutRsc.Texts[26].text,
+                'error'
+            );
         else {
             window.location.href = './quicksearch?query=' + $('#QuickSearch').val();
         }
