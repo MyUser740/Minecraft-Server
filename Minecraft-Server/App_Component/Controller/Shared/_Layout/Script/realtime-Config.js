44412546693d53060282e933c0a444d3d266dedb
axel020207@gmail.com <axel020207@gmail.com>
2020-12-27 21:21:08 +0700

    Creatin SignalR Realtim Notification

---
+++ Minecraft-Server/App_Component/Controller/Shared/_Layout/Script/realtime-Config.js
@@ -0,0 +1,10 @@
+﻿var noti = $.connection.notificationHub;
+
+noti.client.reacive = function (Id,User, From, Message, DateSend) {
+    if($.cookie('usr_Name') === User)
+        $('#notiBox').append('<div class="row"> <div class="col-10"> <strong>' + From + '</strong> </div><div class="col-5"> <p class="text-muted" data-id="' + Id + '"></p></div></div><div class="row"> <span class="badge badge-soft-danger">New</span> <div class="col-20"> <p>' + Message + '</p></div></div>');
+};
+
+$.connection.hub.start().done(function () {
+    console.log('Notification hub Started');
+});
---
+++ Minecraft-Server/Hubs/notificationHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Minecraft_Server.Hubs
+{
+    public class notificationHub : Hub
+    {
+        public void Send(int id, string User, string From, string Message, DateTime DateSend)
+        {
+            Clients.All.Reacive(id,User, From, Message, DateSend);
+        }
+    }
+}
